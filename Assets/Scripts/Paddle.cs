99c133695d2e96a5fb25ca49ed60bc12add4f033
Gabriel Stancu <gabriel.stancu07@yahoo.com>
2020-09-23 13:53:11 +0300

    project files upload

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    // cached references
+    bool isAutoPlayEnabled;
+    Ball ball;
+
+    // Use this for initialization
+    void Start () {
+		isAutoPlayEnabled = FindObjectOfType<GameSession>().IsAutoPlayEnabled();
+        ball = FindObjectOfType<Ball>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+	}
+
+    private float GetXPos()
+    {
+        if (isAutoPlayEnabled)
+        {
+            return ball.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
